59c1571bd3d2807e12edd5d12e524d8eec26b62f
rhdodds <rhdodds@gmail.com>
2015-11-01 21:13:19 -0500

    new cvn example with modern input

---
+++ fracture_models/cvn_threads_only/cleanup.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+\rm wnd* wns* wnt* wnt* wem* state* *batch*mess* save* energy  >& /dev/null
+\rm states* *.neut   >& /dev/null
+\rm *text   >& /dev/null
+\rm cvn.batch_messages   >& /dev/null
+exit
