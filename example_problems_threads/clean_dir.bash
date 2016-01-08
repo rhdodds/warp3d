f675cd6ac6d108ea05fd65d2805872fda84e73f9
Robert Dodds <rhdodds@gmail.com>
2016-01-08 12:00:17 -0500

    for version 17.7.0

--- example_problems_threads/clean_dir.bash
+++ example_problems_threads/clean_dir.bash
@@ -1,50 +1,53 @@
-#!/bin/bash
-touch zqq_db
-touch zqq~
-touch wm_zqq
-touch core_zqq
-touch energy_zqq
-touch fort.zzzzx 
-touch gmon_zxxx
-touch gprof_zzzz
-touch xxxx_messages
-touch zzzz.warp
-touch packets.x
-touch solverxxx
-touch pardisoxxx
-touch test_59_logos_data_a
-touch test_59_logos_data_b
-touch test_59_logos_input_file
-touch test_59.out_a
-touch test_59.out_b
-touch test_60_a_out 
-touch test_60_b_out 
-touch test_60_a_results 
-touch test_60_b_results 
-touch test_60_logos_input_file
-touch zzz.db
-#
-/bin/rm -f  -f *_db 
-/bin/rm -f  -f *~ 
-/bin/rm -f  w* 
-/bin/rm -f  core*
-/bin/rm -f  energy*
-/bin/rm -f  fort.*
-/bin/rm -f  gmon*  
-/bin/rm -f  gprof* 
-/bin/rm -f  *messages
-/bin/rm -f  *.warp 
-/bin/rm -f  packet*
-/bin/rm -f  step_*
-/bin/rm -f  solver*
-/bin/rm -f  pardiso*
-/bin/rm -f test_59_logos* test_59.out*
-/bin/rm -f test_60_a_out 
-/bin/rm -f test_60_b_out 
-/bin/rm -f test_60_a_results 
-/bin/rm -f test_60_b_results 
-/bin/rm -f test_60_logos_input_file
-/bin/rm -f zzz.db
-#
-#
+#!/bin/bash  
+touch zqq_db  >& /dev/null
+touch zqq~  >& /dev/null
+touch wm_zqq  >& /dev/null
+touch core_zqq  >& /dev/null
+touch energy_zqq  >& /dev/null
+touch fort.zzzzx   >& /dev/null
+touch gmon_zxxx  >& /dev/null
+touch gprof_zzzz  >& /dev/null
+touch xxxx_messages  >& /dev/null
+touch zzzz.warp  >& /dev/null
+touch packets.x  >& /dev/null
+touch solverxxx  >& /dev/null
+touch pardisoxxx  >& /dev/null
+touch test_59_logos_data_a  >& /dev/null
+touch test_59_logos_data_b  >& /dev/null
+touch test_59_logos_input_file  >& /dev/null
+touch test_59.out_a  >& /dev/null
+touch test_59.out_b  >& /dev/null
+touch test_60_a_out   >& /dev/null
+touch test_60_b_out   >& /dev/null
+touch test_60_a_results   >& /dev/null
+touch test_60_b_results   >& /dev/null
+touch test_60_logos_input_file  >& /dev/null
+touch zzz.db  >& /dev/null
+#  
+/bin/rm -f  -f *_db   >& /dev/null
+/bin/rm -f  -f *~   >& /dev/null
+/bin/rm -f  w*   >& /dev/null
+/bin/rm -f  core*  >& /dev/null
+/bin/rm -f  energy*  >& /dev/null
+/bin/rm -f  fort.*  >& /dev/null
+/bin/rm -f  gmon*    >& /dev/null
+/bin/rm -f  gprof*   >& /dev/null
+/bin/rm -f  *messages  >& /dev/null
+/bin/rm -f  *.warp   >& /dev/null
+/bin/rm -f  packet*  >& /dev/null
+/bin/rm -f  step_*  >& /dev/null
+/bin/rm -f  solver*  >& /dev/null
+/bin/rm -f  pardiso*  >& /dev/null
+/bin/rm -f test_59_logos* test_59.out*  >& /dev/null
+/bin/rm -f test_60_a_out   >& /dev/null
+/bin/rm -f test_60_b_out   >& /dev/null
+/bin/rm -f test_60_a_results   >& /dev/null
+/bin/rm -f test_60_b_results   >& /dev/null
+/bin/rm -f test_60_logos_input_file  >& /dev/null
+/bin/rm -f zzz.db  >& /dev/null
+/bin/rm -f *.exo >& /dev/null
+/bin/rm -f *.db >& /dev/null
+/bin/rm -f states_header* >& /dev/null
+#  
+#  
 echo "> done..."
