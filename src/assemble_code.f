17d44530f4b116879ecb6ca3259c8d8f0312149c
Robert Dodds <rhdodds@gmail.com>
2016-12-16 10:18:30 -0500

    comment ASSUME_ALIGN directives

--- src/assemble_code.f
+++ src/assemble_code.f
@@ -158,8 +158,8 @@ c
 c      
       integer :: snode, num_ele_on_snode, next_scol_list, ele_on_snode,
      &           totdof, erow, ecol, scol, j
-@!DIR$ ASSUME_ALIGNED eqn_node_map:32, edest:32, iprops:32
-@!DIR$ ASSUME_ALIGNED dof_eqn_map:32, scol_flags:32, scol_list:32      
+c!DIR$ ASSUME_ALIGNED eqn_node_map:32, edest:32, iprops:32
+c!DIR$ ASSUME_ALIGNED dof_eqn_map:32, scol_flags:32, scol_list:32      
 c             
       snode             = eqn_node_map(srow) ! structure node
       num_ele_on_snode  = inverse_incidences(snode)%element_count
@@ -249,9 +249,9 @@ c
      &           num_scols_used, j, totdof, ele_on_snode, erow, ecol,
      &           num_unique_cols, num_ele_on_snode
       integer, parameter :: local_debug = .false.
-@!DIR$ ASSUME_ALIGNED eqn_node_map:32, edest:32, iprops:32
-@!DIR$ ASSUME_ALIGNED dof_eqn_map:32, scol_list:32, k_ptrs:32
-@!DIR$ ASSUME_ALIGNED k_indexes:32      
+c!DIR$ ASSUME_ALIGNED eqn_node_map:32, edest:32, iprops:32
+c!DIR$ ASSUME_ALIGNED dof_eqn_map:32, scol_list:32, k_ptrs:32
+c!DIR$ ASSUME_ALIGNED k_indexes:32      
 c
 c                 simulate assembly of the equilibrium equations directly
 c                 in sparse matrix format, i.e., only the non-zero
@@ -404,7 +404,7 @@ c
       integer :: n, vec(n)
 c
       integer :: l, ir,rra, i, j
-@!DIR$ ASSUME_ALIGNED vec:32
+c!DIR$ ASSUME_ALIGNED vec:32
 c            
       l = n/2 + 1
       ir = n
@@ -470,7 +470,7 @@ c
 c         local
 c
       integer :: eqn_counter, dof 
-@!DIR$ ASSUME_ALIGNED cstmap:32, dof_eqn_map:32, eqn_node_map:32
+c!DIR$ ASSUME_ALIGNED cstmap:32, dof_eqn_map:32, eqn_node_map:32
 c
 c         assign the equation number for each structure dof.
 c         dof with absolute constraints do not appear in the equations.
@@ -574,9 +574,9 @@ c
       integer :: thread_previous_node(max_threads)
       data zero / 0.0d0 /
 c
-@!DIR$ ASSUME_ALIGNED coeff_row:64  
-@!DIR$ ASSUME_ALIGNED eqn_node_map:32, dof_eqn_map:32, k_ptrs:32
-@!DIR$ ASSUME_ALIGNED k_indexes:32, iprops:32, dcp:32         
+c!DIR$ ASSUME_ALIGNED coeff_row:64  
+c!DIR$ ASSUME_ALIGNED eqn_node_map:32, dof_eqn_map:32, k_ptrs:32
+c!DIR$ ASSUME_ALIGNED k_indexes:32, iprops:32, dcp:32         
 c
 c                 assemble the equilibrium equations directly in
 c                 sparse matrix format, i.e., only the non-zero
@@ -710,10 +710,10 @@ c
 c
       logical :: repeated
       data zero / 0.0d0 /
-@!DIR$ ASSUME_ALIGNED k_diag:64, k_coeffs:64, coeff_row:64, emat:64
-@!DIR$ ASSUME_ALIGNED eqn_node_map:32, dof_eqn_map:32, k_ptrs:32
-@!DIR$ ASSUME_ALIGNED k_indexes:32, iprops:32, dcp:32 
-@!DIR$ ASSUME_ALIGNED row_start_index:32, edest:32, local_scol:32    
+c!DIR$ ASSUME_ALIGNED k_diag:64, k_coeffs:64, coeff_row:64, emat:64
+c!DIR$ ASSUME_ALIGNED eqn_node_map:32, dof_eqn_map:32, k_ptrs:32
+c!DIR$ ASSUME_ALIGNED k_indexes:32, iprops:32, dcp:32 
+c!DIR$ ASSUME_ALIGNED row_start_index:32, edest:32, local_scol:32    
 c
 c                 get the structure node number corresponding to this 
 c                 equation.
@@ -957,7 +957,7 @@ c
       logical, parameter :: print_cpu_time = .false.
       real :: start_time, stop_time
 c
-@!DIR$ ASSUME_ALIGNED k_coeffs:64         
+c!DIR$ ASSUME_ALIGNED k_coeffs:64         
 c 
 c       Algorithm is a bit easier than the original one
 c             1) Loop on each equation in parallel
@@ -1037,7 +1037,7 @@ c
      &           bs_finish, bs_range 
 #dbl      double precision, dimension(:,:), pointer :: emat
 #sgl      real, dimension(:,:), pointer :: emat
-@!DIR$ ASSUME_ALIGNED k_coeffs:64,emat:64         
+c!DIR$ ASSUME_ALIGNED k_coeffs:64,emat:64         
 c
 c                 - get structure node number for this equation
 c                 - number of element connected to snode
@@ -1182,7 +1182,7 @@ c
       integer, dimension (:,:), contiguous, pointer :: edest
 c
       integer :: i, elem, totdof, blk, rel_elem, dof      
-c@!DIR$ ASSUME_ALIGNED table:32, elem_list:32, iprops:32, edest:32
+cc!DIR$ ASSUME_ALIGNED table:32, elem_list:32, iprops:32, edest:32
 c
       do i = 1, list_length
        elem = elem_list(i)   ! absolute element number
