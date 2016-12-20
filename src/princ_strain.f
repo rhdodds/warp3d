fb0dec4546f7ec5952f9910a22746e1123370d7f
Robert Dodds <rhdodds@gmail.com>
2016-12-19 21:56:26 -0500

    removal of warp3d preprocessor

--- src/princ_strain.f
+++ src/princ_strain.f
@@ -16,15 +16,13 @@ c     ****************************************************************
 c
       subroutine princ_strain( results, nrowd, num )
       implicit integer (a-z)
-$add param_def
-#dbl      double precision
-#sgl      real
+      include 'param_def'
+      double precision
      &     results(nrowd,*)
 c
 c                    locally allocated
 c
-#dbl      double precision
-#sgl      real
+      double precision
      &  temp_strain(nstr), wk(ndim), ev(nstr), evec(ndim,ndim), half
 c
       data  half / 0.5 / 
