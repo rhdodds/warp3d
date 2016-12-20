bd0126bdfb9835957012b7f95c01eb242d90b1d5
Robert Dodds <rhdodds@gmail.com>
2016-12-19 21:56:22 -0500

    removal of warp3d preprocessor

--- src/mod_elblk.f
+++ src/mod_elblk.f
@@ -17,26 +17,23 @@ c
       module elblk_data
 c     -----------------
 c
-$add param_def
+      include 'param_def'
 c
 c                stresses, strains, histories, etc. for a block
 c
-#dbl      double precision
-#sgl      real
+      double precision
      & ddtse(mxvl,nstr,mxgp),
      & urcs_blk_n(mxvl,nstrs,mxgp),
      & urcs_blk_n1(mxvl,nstrs,mxgp), rot_blk_n1(mxvl,9,mxgp)
       save rtse, ddtse, urcs_blk_n, urcs_blk_n1, rot_blk_n1
 c
-#dbl      double precision,
-#sgl      real,
+      double precision,
      &       dimension(:,:,:), save, allocatable ::
      &        elem_hist, elem_hist1
 c
 c                internal forces, transformations for block of elements
 c
-#dbl      double precision
-#sgl      real
+      double precision
      & elestr(mxvl,mxoupr,mxoupt)
       save elestr
 c
