b4a7e9e2c559a3c77aa2862ca53e7e365e7ae713
Robert Dodds <rhdodds@gmail.com>
2017-01-29 14:36:39 -0500

    updating to support mpi execution + flat output files

--- src/oumkcv.f
+++ src/oumkcv.f
@@ -4,7 +4,7 @@ c     *                      subroutine oumkcv                       *
 c     *                                                              *
 c     *                       written by : rhd                       *
 c     *                                                              *
-c     *                   last modified : 3/10/04                    *
+c     *                   last modified : 01/27/2017 rhd             *
 c     *                                                              *
 c     *     this subroutine avarages element strains or stresses     *
 c     *     at the gauss points to define a single set over the      *
@@ -13,51 +13,66 @@ c     *                                                              *
 c     ****************************************************************
 c
 c           
-      subroutine oumkcv( span, ngp, stress, num_short_stress,
+      subroutine oumkcv( span, ngp, do_stresses, num_short_stress,
      &                   num_short_strain )
       use elblk_data, only : elestr
-      implicit integer (a-z)
+      implicit none
+c      
       include 'param_def'
-      logical stress
+      integer :: span, ngp, num_short_stress, num_short_strain
+      logical :: do_stresses
 c
 c                       local declarations
 c
-      double precision
-     &     temvals(mxvl,mxoupr), zero, rngp
-      data zero /0.0/
+      integer :: i, j, k, num_vals
+      double precision :: temvals(mxvl,mxoupr), rngp
+      double precision, parameter :: zero = 0.0d00
 c
 c                       set the actual number of primary output
 c                       values. can be different for stresses
 c                       and strains
 c      
       num_vals = num_short_strain
-      if ( stress )  num_vals = num_short_stress
+      if( do_stresses )  num_vals = num_short_stress
 c      
 c                       1. zero accumulation array for average.
 c          
-      do j = 1, num_vals
-        do i = 1, span
-          temvals(i,j) = zero
-        end do
-      end do
+      temvals = zero
 c
 c                       2. build summed values over gauss points
 c                          for each stress/strain value. do all
 c                          elements in block at same time
 c          
-      do k = 1, ngp
+      if( ngp .eq. 8 ) then
+c        
+       do k = 1, 8
         do j = 1, num_vals
+!DIR$ LOOP COUNT MAX=128
           do i = 1, span
              temvals(i,j) = temvals(i,j) + elestr(i,j,k)
           end do   
         end do
-      end do   
+       end do 
+c         
+      else  ! not 8 int points
+c
+       do k = 1, ngp
+        do j = 1, num_vals
+!DIR$ LOOP COUNT MAX=128
+          do i = 1, span
+             temvals(i,j) = temvals(i,j) + elestr(i,j,k)
+          end do   
+        end do
+       end do   
+c
+      end if
 c
 c                       3. compute average value of each strain/stress
 c                          value for each element.
 c 
-      rngp = dble(ngp)         
+      rngp = dble(ngp)    
       do j = 1, num_vals
+!DIR$ LOOP COUNT MAX=128
         do i = 1, span
            temvals(i,j) = temvals(i,j) / rngp
         end do 
@@ -66,13 +81,29 @@ c
 c                       4. put average values within element at every
 c                          gauss point of element
 c          
-      do k = 1, ngp
-        do j = 1, num_vals
-          do i = 1, span
-             elestr(i,j,k) = temvals(i,j)
-          end do   
-        end do
-      end do   
+      if( ngp .eq. 8 ) then
+c
+         do k = 1, 8
+           do j = 1, num_vals
+!DIR$ LOOP COUNT MAX=128
+             do i = 1, span
+                elestr(i,j,k) = temvals(i,j)
+             end do   
+           end do
+         end do   
+c
+      else
+c
+         do k = 1, ngp
+           do j = 1, num_vals
+!DIR$ LOOP COUNT MAX=128
+             do i = 1, span
+                elestr(i,j,k) = temvals(i,j)
+             end do   
+           end do
+         end do   
+c         
+      end if
 c
       return
       end
