53d06c294b16a60a982fe6808ef6937a4788828b
Robert Dodds <rhdodds@gmail.com>
2017-01-29 14:36:39 -0500

    updating to support mpi execution + flat output files

--- src/oupele.f
+++ src/oupele.f
@@ -4,34 +4,39 @@ c     *                      subroutine oupele                       *
 c     *                                                              *
 c     *                       written by : kck                       *
 c     *                                                              *
-c     *                   last modified : 3/11/04 (rhd)              *
+c     *                   last modified : 1/19/2017 rhd              *
 c     *                                                              *
-c     *     this subroutine copies the strain/stress results at the  *
+c     *     copy strain/stress results at the                        *
 c     *     element center into the data array for all element       *
 c     *     results.                                                 *
 c     *                                                              *
 c     ****************************************************************
 c
 c
-      subroutine oupele( span, num_strain, num_stress, stress,
+      subroutine oupele( span, num_strain, num_stress, do_stresses,
      &                   elem_results, nrowd )
       use elblk_data, only : elestr
-      implicit integer (a-z)
-      logical stress
+      implicit none
+c      
       include 'param_def'
-      double precision
-     &  elem_results(nrowd,*)
+c
+      integer :: span, num_strain, num_stress, nrowd      
+      logical :: do_stresses
+      double precision :: elem_results(nrowd,*)
+c
+      integer :: num_vals, k, i   
 c
 c                       copy element results into global
 c                       data structure for element center results.
 c
       num_vals = num_strain
-      if ( stress ) num_vals = num_stress
+      if( do_stresses ) num_vals = num_stress
 c
       do k = 1, num_vals
-            do i = 1, span
-               elem_results(i,k) =  elestr(i,k,1)
-            end do
+!DIR$ LOOP COUNT MAX=128
+        do i = 1, span
+          elem_results(i,k) =  elestr(i,k,1)
+        end do
       end do
 c
       return
