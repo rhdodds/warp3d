f5ac975d6db66b1e69777ce270df5d43178ed5a7
Robert Dodds <rhdodds@gmail.com>
2017-02-21 22:05:18 -0500

    revised driver for MPI+threads tests

--- verification/run_mpi_tests.bash
+++ verification/run_mpi_tests.bash
@@ -1,44 +1,61 @@
 #/bin/bash
 
 function run_all {
-hybrid-test-short
-hybrid-test-long
-threads-hybrid-test
+test1
+hybrid-test-2test3
+test4
+test5
+test6
 }
 
-function hybrid-test-short {
+function test1 {
 echo -e "\n>>> Hypre solver. Crk growth w/ cohesive. C(T): ~1.5 mins"
 echo      "    ====================================================="
-cd hybrid-test-short
+cd hybrid_test_1
 bash ./run_tests_and_check  2>/dev/null # fixes lower shell issue
 cd ..
 }
 
-function hybrid-test-long {
+function test2 {
 echo -e "\n>>> Hypre sovler. Crk growth w/ cohesive. C(T) ~8 mins"
 echo      "    =================================================="
-cd hybrid-test-long
+cd hybrid_test_2
 bash ./run_tests_and_check 2>/dev/null
 cd ..
 }
 
-function threads-hybrid-test {
-echo -e "\n>>> MPI + MKL sparse iterative (Pressure vessel)"
-echo      "    ============================================"
-cd threads-hybrid-test
+function test3 {
+echo -e "\n>>> Pardiso iterative. Assemble/solve on root (Pressure vessel)"
+echo      "    =========================================================="
+cd hybrid_test_3
 bash ./run_tests_and_check 2>/dev/null
 cd ..
 }
 
+function test4 {
+echo -e "\n>>> MPI (short)soluton + combine partial results files"
+echo      "    =================================================="
+cd hybrid_test_4
+bash ./run_tests_and_check 2>/dev/null
+cd ..
+}
 
-
-function MPI_combine {
-echo -e "\n>>> MPI soluton + combine partial results files"
+function test5 {
+echo -e "\n>>> Cluster Pardiso asymmetric (Pressure vessel)"
 echo      "    ==========================================="
-cd hybrid-test-MPI-combine
+cd hybrid_test_5
 bash ./run_tests_and_check 2>/dev/null
 cd ..
 }
+
+function test6 {
+echo -e "\n>>> Cluster Pardiso symmetric (Pressure vessel)"
+echo      "    ==========================================="
+cd hybrid_test_6
+bash ./run_tests_and_check 2>/dev/null
+cd ..
+}
+
 
 #*********************************************************
 #*                                                       *
@@ -46,7 +63,6 @@ cd ..
 #*                                                       *
 #*********************************************************
 #
-
 # Make sure WARP3D_HOME is set
 [ -z "$WARP3D_HOME" ] && echo "Need to set WARP3D_HOME before proceeding." && exit 1
 
@@ -73,32 +89,38 @@ fi
 # Export everything
 export warp3d=$name
 export ranks_warp3d=4
-export threads_warp3d=2
+export threads_warp3d=6
 
 # Print the menu
 printf "\n> Select a problem...\n\n"
-m_testhybrid_short="Hypre solver (Crk growth w/ coheive in  C(T)): Short (2 mins)"
-m_testhybrid_long="Hypre solver (Crk growth w/ coheive in  C(T)): Long (8-10 mins)"
-m_testthreadshybrid="MPI + MKL sparse iterative (Pressure vessel)"
-m_testMPIcombine="MPI solution + test MPI combine partial result files: Short (0.2 min)"
+m_test1="Hypre solver (Crk growth w/ coheive in  C(T)): Short (2 mins)"
+m_test2="Hypre solver (Crk growth w/ coheive in  C(T)): Long (8-10 mins)"
+m_test3="Pardiso (threaded) iterative - pressure vessel: (1-2 mins)"
+m_test4="Test MPI combine partial result files: Short (0.2 min)"
+m_test5="Cluster Pardiso asymmetric - pressure vessel: (1-2 mins)"
+m_test6="Cluster Pardiso symmetric - pressure vessel: (1-2 mins)"
 #
 all="All problems"
 quit="Quit"
 PS3="Enter your choice (<return> to repeat menu): "
-select menu_list in "$all" "$m_testhybrid_short" "$m_testhybrid_long" \
-    "$m_testthreadshybrid" "$m_testMPIcombine" "$quit"
+select menu_list in "$all" "$m_test1" "$m_test2" \
+    "$m_test3" "$m_test4" "$m_test5"  "$m_test6"  "$quit"
 do
       case $menu_list in
             $all)
                   run_all;;
-            $m_testhybrid_short)
-                  hybrid-test-short;;
-            $m_testhybrid_long)
-                  hybrid-test-long;;
-            $m_testthreadshybrid)
-                  threads-hybrid-test;;
-            $m_testMPIcombine)
-                  MPI_combine;;
+            $m_test1)
+                  test1;;
+            $m_test2)
+                  test2;;
+            $m_test3)
+                  test3;;
+            $m_test4)
+                  test4;;
+            $m_test5)
+                  test5;;
+            $m_test6)
+                  test6;;
             $quit)
                   break;;
             *) printf "You can enter only 1, .....\n";;
