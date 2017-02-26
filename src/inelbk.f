7c3cbbeabd0f2a49c6fe932a39eb4a3f4c6a0113
Robert Dodds <rhdodds@gmail.com>
2017-02-25 22:05:02 -0500

    add auto domain assignment and much cleanup

--- src/inelbk.f
+++ src/inelbk.f
@@ -5,7 +5,7 @@ c     *                      subroutine inelbk                       *
 c     *                                                              *
 c     *                       written by : rhd                       *
 c     *                                                              *
-c     *                   last modified : 6/13/2013 RHD              *
+c     *                   last modified : 2/27/2017 rhd              *
 c     *                                                              *
 c     *     input of element blocking information. computations for  *
 c     *     element blocking and optionally domains if requested     *
@@ -13,20 +13,21 @@ c     *                                                              *
 c     ****************************************************************
 c
 c
-c
-      subroutine inelbk(sbflg1,sbflg2)
-      implicit integer (a-z)
+      subroutine inelbk( sbflg1, sbflg2 )
+      implicit none
       include 'common.main'
+c
+      logical :: sbflg1,sbflg2
+c       
+      integer :: i, auto_size, ival, blk, param, proc, span, felem,
+     &           elem, msgcnt, dum, nc
+      integer, allocatable :: checkblk(:)
       real :: dumr
       double precision :: dumd
-      character :: dums, item*80
-      logical :: sbflg1,sbflg2
-      logical :: first_proc, auto_blking, ok,
-     &           display, auto_domains
+      logical :: auto_blking, ok, display, auto_domains
       logical, external :: matchs, integr, endcrd
-      integer, allocatable :: checkblk(:)
-      data first_proc /.true./
-c
+      logical, save :: first_proc = .true.
+      character :: dums*1, item*80
 c
 c                       if the subroutine has been previously
 c                       entered and exited, then there was an
@@ -36,9 +37,7 @@ c                       absence of the block number. under this
 c                       circumstance, print an error message and
 c                       continue with input.
 c
-      if( sbflg1 ) then
-         call errmsg(43,dum,dums,dumr,dumd)
-      end if
+      if( sbflg1 ) call errmsg(43,dum,dums,dumr,dumd)
 c
 c                       initialize blocking data structures.
 c
@@ -49,39 +48,174 @@ c
          elblks(2,i) = 0   ! optional domain  number for element
          elblks(3,i) = -1  ! doesn't seem to be used elsewhere
       end do
-      if( numprocs .eq. 1 ) then
+c      
+      if( numprocs == 1 ) then ! mpi w/ one rank
          do i = 1, mxnmbl
             elblks(2,i) = 0
          end do
-      else
+      else  ! no domains
          do i = 1, mxnmbl
             elblks(2,i) = -1
          end do
       end if
 c
-c                       user request for automatic blocking is
-c                       given on blocking command.
-c
-      auto_size = mxvl  !  from param_def
-      auto_blking = .false.
-      display = .false.
+      auto_size    = mxvl  !  from param_def
+      auto_blking  = .false.
+      display      = .false.
       auto_domains = .false.
-      auto_num_domains = 0  ! just for code debugging
-c                             in real mpi run will be
-c                             set = numprocs shortly
-      if( endcrd(dumr) ) go to 200 ! user-defined blocking
 c
+      if( endcrd(dumr) ) then ! only word blocking on command
+         call inelbk_user
+      else
+         call inelbk_automatic ! probably automatic blocking/domains
+         call inelbk_user ! just in case use  still gives blocking
+      end if
+c
+c                      end of blocking input. run checks to make
+c                      sure that all elements have been assigned to a block
+c                      and that elements are assigned to only one block.
+c                      tries to find various errors that users can make !
+c
+      allocate( checkblk(noelem) )
+      checkblk(1:noelem) = 0
+c
+      do blk = 1, nelblk
+       felem = elblks(1,blk)
+       span  = elblks(0,blk)
+       do i = 1, span
+         elem = felem + i - 1
+         checkblk(elem) = checkblk(elem) + 1
+       end do
+      end do
+c
+      msgcnt = 0
+      do elem = 1, noelem
+        if( checkblk(elem) .ne. 1 ) then
+          if( msgcnt == 0 ) write(out,9000)
+          if( checkblk(elem) > 1 ) write(out,9010) elem
+          if( checkblk(elem) == 0 ) write(out,9020) elem
+          msgcnt = msgcnt + 1
+        end if
+      end do
+c
+      deallocate( checkblk )
+c
+      if( msgcnt > 0 ) then
+         write(out,9030)
+         call die_abort
+      end if
+c
+      return
+c
+ 9000 format(//,'>>>> Fatal Error: inconsistent blocking for elements' )
+ 9010 format(10x,'Element: ',i6,' assigned to more than 1 block' )
+ 9020 format(10x,'Element: ',i6,' is not assigned to a block' )
+ 9030 format(//,'>>>> Job terminated due to fatal blocking errors' )
+c
+      contains
+c     ========
+c     ****************************************************************
+c     *                                                              *
+c     *      contains:   inelblk_user                                *
+c     *                                                              *
+c     ****************************************************************
+c
+      subroutine inelbk_user 
+      implicit none
+c      
+      do while ( .true. )
+      call readsc
+c
+c                      read in element block. if no block is
+c                      given, then either the block has been
+c                      forgotten or blocking input has ended.
+c                      in either case, set sbflg1 to true and
+c                      exit the subroutine. if the block has
+c                      been forgotten, then the rest of the
+c                      line will be skipped.
+c
+      if( .not.integr(blk) ) exit
+c
+c                       valid block number ?
+c
+      if( blk > mxnmbl ) then
+         param = blk
+         call errmsg(74,param,dums,dumr,dumd)
+         cycle
+      end if
+      if( blk < 0 ) then
+         param = blk
+         call errmsg(76,param,dums,dumr,dumd)
+         cycle
+      end if
+c
+c                      read in the block span and the first
+c                      element in the block.
+c
+      if( .not. integr(span) ) then
+         call errmsg(77,dum,dums,dumr,dumd)
+         cycle
+      end if
+c
+      if( (span < 1) .or. (span > mxvl) ) then
+         call errmsg(185,span,dums,dumr,dumd)
+         cycle
+      end if
+c
+      if( .not. integr(felem) ) then
+         call errmsg(159,dum,dums,dumr,dumd)
+         cycle
+      end if
+c
+c                      if present, read in processor assignment
+c                      for given block.
+c
+      if( integr(proc) ) then
+         if( proc .lt. 0 ) then
+            call errmsg( 294, dum, dums, dumr, dumd )
+            proc = 0
+         end if
+         if( use_mpi ) then
+            elblks(2,blk)= proc
+         else
+            elblks(2,blk)= 0
+         end if
+      end if
+c
+c                      set the appropriate block data structures.
+c
+      elblks(0,blk) = span
+      elblks(1,blk) = felem
+      nelblk        = max(nelblk,blk)
+c
+      end do
+c
+      sbflg1 = .true.
+      sbflg2 = .true.
+c      
+      return
+      end subroutine inelbk_user
+c      
+c     ****************************************************************
+c     *                                                              *
+c     *      contains:   inelblk_automatic                           *
+c     *                                                              *
+c     ****************************************************************
+c
+      subroutine inelbk_automatic 
+      implicit none
+c      
       if( matchs( 'automatic',4) ) then
          auto_blking = .true.
       else
-          write(out,9040)
-          go to 200
+          write(out,9040)  ! unknown blocking option
+          return
       end if
 c
       if( matchs('size',4) ) then
         if( matchs('=',1) ) call splunj
         if( integr( ival ) ) then
-          ok = ival .ge. 1  .and. ival .le. mxvl
+          ok = ival >= 1  .and. ival <= mxvl
           if( ok ) then
             auto_size = ival
           else
@@ -96,10 +230,9 @@ c
 c
       if( matchs('domains',4) ) then
         auto_domains = .true.
-        if( matchs('=',1) ) call splunj
-        if( integr( ival ) ) auto_num_domains = ival
+        if( matchs('automatic',4) ) call splunj
       end if
-
+c
       if( matchs('display',4) ) then
         display = .true.
       elseif( endcrd() ) then
@@ -112,129 +245,12 @@ c
 c
       if( matchs('display',4) ) display = .true.
 c
-      call inelbk_simple_blocking( auto_size, auto_domains,
-     &                             auto_num_domains, display )
-c
- 200  continue
-      call readsc
-c
-c                      read in element block. if no block is
-c                      given, then either the block has been
-c                      forgotten or blocking input has ended.
-c                      in either case, set sbflg1 to true and
-c                      exit the subroutine. if the block has
-c                      been forgotten, then the rest of the
-c                      line will be skipped.
-c
-      if( .not.integr(blk) ) go to 9999
-c
-c                       check that the block input does not exceed
-c                       the maximum number of blocks.
-c
-      if( blk .gt. mxnmbl ) then
-         param = blk
-         call errmsg(74,param,dums,dumr,dumd)
-         go to 200
-      end if
-c
-c                       check that the block input is not negative.
-c
-      if( blk .lt. 0 ) then
-         param = blk
-         call errmsg(76,param,dums,dumr,dumd)
-         go to 200
-      end if
-c
-c                      read in the block span and the first
-c                      element in the block.
-c
-      if( .not. integr(span) ) then
-         call errmsg(77,dum,dums,dumr,dumd)
-         go to 200
-      end if
-c
-      if ( (span .lt. 1)  .or.  (span .gt. mxvl) ) then
-         call errmsg(185,span,dums,dumr,dumd)
-         goto 200
-      end if
-c
-      if( .not. integr(felem) ) then
-         call errmsg(159,dum,dums,dumr,dumd)
-         go to 200
-      end if
-c
-c                      if present, read in processor assignment
-c                      for given block.
-c
-      if( integr(proc) ) then
-c
-         if( proc .lt. 0 ) then
-            call errmsg( 294, dum, dums, dumr, dumd )
-            proc = 0
-         end if
-c
-         if( use_mpi ) then
-            elblks(2,blk)= proc
-         else
-            elblks(2,blk)= 0
-         end if
-c
-      end if
-c
-c                      set the appropriate block data structures.
-c
-      elblks(0,blk) = span
-      elblks(1,blk) = felem
-      nelblk        = max(nelblk,blk)
-c
-      go to 200
-c
- 9999 sbflg1 = .true.
-      sbflg2 = .true.
-c
-c                      end of blocking input. run checks to make
-c                      sure that all elements have been assigned to a block
-c                      and that elements are assigned to only one block.
-c                      tries to find various errors that users can make!
-c
-      allocate(checkblk(noelem))
-c
-      do elem = 1, noelem
-       checkblk(elem) = 0
-      end do
-c
-      do blk = 1, nelblk
-       felem = elblks(1,blk)
-       span  = elblks(0,blk)
-       do i = 1, span
-         elem = felem + i - 1
-         checkblk(elem) = checkblk(elem) + 1
-       end do
-      end do
-c
-      msgcnt = 0
-      do elem = 1, noelem
-        if( checkblk(elem) .ne. 1 ) then
-          if( msgcnt .eq. 0 ) write(out,9000)
-          if( checkblk(elem) .gt. 1 ) write(out,9010) elem
-          if( checkblk(elem) .eq. 0 ) write(out,9020) elem
-          msgcnt = msgcnt + 1
-        end if
-      end do
-c
-      deallocate(checkblk)
-c
-      if( msgcnt .gt. 0 ) then
-         write(out,9030)
-         call die_abort
-      end if
-c
+c                      do auto blocking and optionally domains
+c        
+      call inelbk_simple_blocking( auto_size, auto_domains, display )
+c             
       return
 c
- 9000 format(//,'>>>> Fatal Error: inconsistent blocking for elements' )
- 9010 format(10x,'Element: ',i6,' assigned to more than 1 block' )
- 9020 format(10x,'Element: ',i6,' is not assigned to a block' )
- 9030 format(//,'>>>> Job terminated due to fatal blocking errors' )
  9040 format(//,1x
      &'>>>> Unknown option on blocking command - command ignore')
  9050 format(//,1x,'>>>> Invalid blocksize: ',i4,' using: ',i4,
@@ -242,15 +258,16 @@ c
  9060 format(//,1x,'>>>> Unrecognized blocking option: ',a20)
  9070 format(1x,   '     option ignored')
  9080 format(//,1x,'>>>> Unrecognized size option: ',a20)
-c
-      end
+c      
+      end subroutine inelbk_automatic  
+      end subroutine inelbk
 c     ****************************************************************
 c     *                                                              *
 c     *          subroutine inelbk_simple_blocking                   *
 c     *                                                              *
 c     *                       written by : rhd                       *
 c     *                                                              *
-c     *                   last modified : 10/2/12 RHD                *
+c     *             last modified : 2/27/2017 rhd                    *
 c     *                                                              *
 c     *     automatic generation of element blocking: simple version *
 c     *     for threaded w/o vectorized blocking. optional           *
@@ -259,12 +276,11 @@ c     *                                                              *
 c     ****************************************************************
 c
       subroutine inelbk_simple_blocking( auto_size, auto_domains,
-     &                       auto_num_domains, display )
+     &                                   display )
       implicit none
       include 'common.main'
       integer :: auto_size, auto_num_domains
       logical :: auto_domains, display
-c
 c
 c                     locals also visible by contains
 c
@@ -277,8 +293,9 @@ c
       integer ::  blk_matmodel, matmodel, blk_eletype, eletype,
      &            blk_intord, intord, blk_intnum, intnum, i, 
      &            blk_matnum, matnum, current_size, felem, 
-     &            element, param, idomain, hblk, lblk, blks_per_domain
-
+     &            element, param, idomain, hblk, lblk, blks_per_domain,
+     &            blk, domain
+      integer ::  counts_blks_ea_domain(0:max_procs-1,2)
       character(len=1) :: dums
       double precision :: dumd
       real :: dumr
@@ -301,8 +318,9 @@ c
       felem        = 1
       elblks(1,1)  = 1  ! first element in block
       elblks(0,1)  = 1  ! number elements in block
-      if( noelem .eq. 1 ) go to 200
-      call inelbk_load_block_props( 1 )
+      if( noelem == 1 ) return
+c      
+      call inelbk_load_block_props( 1 ) ! more than 1 elements
 c
 c                     1. element fits in current block?
 c                     2. if yes, is it compatible with elements now
@@ -338,24 +356,27 @@ c
          elblks(0,nelblk)  = 1
          current_size       = 1
          call inelbk_load_block_props( felem )
-      end do
-
-      if( .not. auto_domains ) go to 200
-      write(out,*) '.. auto_num_domains: ' , auto_num_domains
-      if( use_mpi ) auto_num_domains = numprocs
-      lblk = 1
-      blks_per_domain = nelblk / auto_num_domains
-      write(out,*) '.. blks_per_domain: ',blks_per_domain
-      do idomain = 1, auto_num_domains
-        hblk = min( nelblk, idomain * blks_per_domain )
-        if( idomain .eq. auto_num_domains ) hblk = nelblk
-        elblks(2,lblk:hblk) = idomain - 1
-        lblk = hblk + 1
-      end do
+      end do ! on element
+c
+c                     simplest assignment of blocks to domains. 
+c                     probably works well if user optimized element
+c                     number in code like Patran
+c
+      if( auto_domains ) then
+        auto_num_domains = 1
+        if( use_mpi ) auto_num_domains = numprocs
+        lblk = 1
+        blks_per_domain = nelblk / auto_num_domains
+        do idomain = 1, auto_num_domains
+          hblk = min( nelblk, idomain * blks_per_domain )
+          if( idomain .eq. auto_num_domains ) hblk = nelblk
+          elblks(2,lblk:hblk) = idomain - 1
+          lblk = hblk + 1
+        end do
+      end if  
 c
 c                     display blocking table if requested
 c
- 200  continue
       if( .not. display ) return
       write(out,9000) nelblk, auto_size
       write(out,9010)
@@ -363,20 +384,43 @@ c
         write(out,9020) i, elblks(1,i),  elblks(0,i), elblks(2,i)
       end do
       write(out,*) ' '
+c
+      if( .not. use_mpi ) return      
+      counts_blks_ea_domain(0:max_procs-1,1:2) = 0
+      do blk = 1, nelblk
+       domain = elblks(2,blk)
+       counts_blks_ea_domain(domain,1) = 
+     &        counts_blks_ea_domain(domain,1) + 1
+       counts_blks_ea_domain(domain,2) = 
+     &        counts_blks_ea_domain(domain,2) + elblks(0,domain)
+      end do
+      write(out,9030)
+      write(out,9040)
+      do i = 0, numprocs-1
+        write(out,9050)  i, counts_blks_ea_domain(i,1), 
+     &                   counts_blks_ea_domain(i,2)
+      end do
+      write(out,*) ' '
+c
       return
 c
  9000 format(/,'>> Generated element blocking table:',
      & /,      1x,'  number of blocks, target size: ',i7,i5)
  9010 format(/,5x,
      &'block        1st element in blk        # elements in block',
-     &10x,'assigned domain' )
+     &5x,'assigned domain' )
  9020 format(1x,i8, 10x,i9,25x,i4,10x,i6)
+ 9030 format(/,'>> Domain statistics:')
+ 9040 format(/,5x,'domain     # element blks    total # elements' )
+ 9050 format(5x,i5,8x,i4,13x,i7)
 
+ 
       contains
+c     ========      
 
 c ********************************************************************
 c *                                                                  *
-c *    routine inelbk_load_block_props                               *
+c *    contains: inelbk_load_block_props                             *
 c *                                                                  *
 c ********************************************************************
 c
@@ -402,7 +446,7 @@ c
 
 c ********************************************************************
 c *                                                                  *
-c *    routine inelbk_load_elem_props                                *
+c *    contains: routine inelbk_load_elem_props                      *
 c *                                                                  *
 c ********************************************************************
 c
@@ -428,7 +472,7 @@ c
 
 c ********************************************************************
 c *                                                                  *
-c *    routine inelbk_chk_match                                      *
+c *    contains: routine inelbk_chk_match                            *
 c *                                                                  *
 c ********************************************************************
 c
