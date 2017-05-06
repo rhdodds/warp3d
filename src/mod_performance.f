63410e96d17f1b6ec2ab9d29caca287febe249c8
Robert Dodds <rhdodds@gmail.com>
2017-05-06 08:17:49 -0400

    added internal timing feature

--- src/mod_performance.f
+++ src/mod_performance.f
@@ -4,9 +4,9 @@ c     *                      module performance_data                 *
 c     *                                                              *
 c     *                       written by : mcm                       *
 c     *                                                              *
-c     *                   last modified : 06/10/2013 rhd             *
+c     *                   last modified : 04/27/2017 rhd             *
 c     *                                                              *
-c     *      stores profiling data (right now, just the wall time)   *
+c     *                      stores various profiling data           *
 c     *                                                              *
 c     ****************************************************************
 
@@ -18,21 +18,75 @@ c
       double precision, save :: start_assembly_step, assembly_total
       integer, save :: ntimes_assembly
 c
+      real, save, private :: time_pardiso, time_warp,
+     &                       start_run_pardiso     
+c
       contains
-            subroutine t_start_assembly(tstart)
-                  implicit none
-                  double precision :: omp_get_wtime, tstart
-
-                  tstart = omp_get_wtime()
+c      
+        subroutine t_start_assembly( tstart )
+        implicit none
+        double precision :: omp_get_wtime, tstart
 c
-            end subroutine
+        tstart = omp_get_wtime()
+        return
 c
-            subroutine t_end_assembly(tstore,tstart)
-                  implicit none
-                  double precision :: omp_get_wtime,tstore,tstart
-
-                  tstore = tstore + (omp_get_wtime()-
-     &                   tstart)
-            end subroutine
+        end subroutine
 c
+        subroutine t_end_assembly( tstore, tstart )
+        implicit none
+        double precision :: omp_get_wtime, tstore, tstart
+c
+        tstore = tstore + (omp_get_wtime()-tstart)
+c        
+        return
+        end subroutine
+        
+        subroutine t_init_performance
+        implicit none
+c       
+        time_pardiso = 0.0
+        time_warp = 0.0
+c        
+        return
+        end subroutine
+c
+        subroutine t_performance_eoj( t1 )
+        implicit none
+        real :: t1, t2
+c       
+        call cpu_time( t2 )
+        t1 = t2 - time_warp
+c        
+        return
+        end subroutine
+        
+        subroutine t_performance_start_pardiso
+        implicit none
+c       
+        call cpu_time( start_run_pardiso )
+c        
+        return
+        end subroutine
+        
+        subroutine t_performance_end_pardiso
+        implicit none
+        real :: t1
+c       
+        call cpu_time( t1 )
+        time_pardiso = time_pardiso + (t1 - start_run_pardiso)
+c        
+        return
+        end subroutine
+        
+        subroutine t_performance_eoj_pardiso( t1 )
+        implicit none
+        real :: t1
+c       
+        t1 = time_pardiso
+c        
+        return
+        end subroutine
+        
+        
+        
       end module performance_data
