a68c2d3cef55d1f3f90bbcad1c79e3056ead94c8
Robert Dodds <rhdodds@gmail.com>
2017-06-06 21:36:36 -0400

    remove commons, improve threading

--- src/packets.f
+++ src/packets.f
@@ -1,75 +1,75 @@
-c     ****************************************************************
-c     *                                                              *
-c     *                      subroutine open_packets_file            *
-c     *                                                              *
-c     *                       written by : rhd                       *
-c     *                                                              *
-c     *                   last modified : 10/14/00                   *
-c     *                                                              *
-c     *                                                              *
-c     ****************************************************************
-c
-c
-c
-      subroutine open_packets_file(msg_flag)
-      use main_data, only : output_packets, packet_file_name,
-     &                      packet_file_no
-      implicit integer (a-z)
-      include 'common.main'
-      real dumr
-      character :: dums
-      double precision
-     &   dumd
-      logical found, msg_flag 
-
-c
-c                        open the existing binary file of packet 
-c                        results if name found in restart file.
-c
-      if ( output_packets ) then
-         inquire( file=packet_file_name, exist=found )
-         if( found .and. msg_flag )
-     &                   call errmsg2( 26, dum, packet_file_name,
-     &                                 dumr, dumd )
-         open(unit=packet_file_no, file=packet_file_name,
-     &        access='sequential',form='unformatted',
-     &        status='unknown',position='append')
-      end if
-      return
-      end
-c     ****************************************************************
-c     *                                                              *
-c     *                      subroutine close_packets_file           *
-c     *                                                              *
-c     *                       written by : rhd                       *
-c     *                                                              *
-c     *                   last modified : 10/14/00                   *
-c     *                                                              *
-c     *                                                              *
-c     ****************************************************************
-c
-c
-c
-      subroutine close_packets_file(msg_flag)
-      use main_data, only : packet_file_name, packet_file_no
-      implicit integer (a-z)
-      include 'common.main'
-      real dumr
-      character :: dums
-      double precision
-     &   dumd
-      logical connected, msg_flag
-c
-c
-c                        open the existing binary file of packet 
-c                        results if name found in restart file.
-c
-      inquire( unit=packet_file_no, opened=connected )
-      if ( connected ) then
-        close(unit=packet_file_no,status='keep')
-        if ( msg_flag )
-     &    call errmsg2( 27, dum, packet_file_name, dumr, dumd ) 
-      end if 
-c
-      return
-      end
+c     ****************************************************************          
+c     *                                                              *          
+c     *                      subroutine open_packets_file            *          
+c     *                                                              *          
+c     *                       written by : rhd                       *          
+c     *                                                              *          
+c     *                   last modified : 10/14/00                   *          
+c     *                                                              *          
+c     *                                                              *          
+c     ****************************************************************          
+c                                                                               
+c                                                                               
+c                                                                               
+      subroutine open_packets_file(msg_flag)                                    
+      use global_data ! old common.main
+      use main_data, only : output_packets, packet_file_name,                   
+     &                      packet_file_no                                      
+      implicit integer (a-z)                                                    
+      real dumr                                                                 
+      character :: dums                                                         
+      double precision                                                          
+     &   dumd                                                                   
+      logical found, msg_flag                                                   
+                                                                                
+c                                                                               
+c                        open the existing binary file of packet                
+c                        results if name found in restart file.                 
+c                                                                               
+      if ( output_packets ) then                                                
+         inquire( file=packet_file_name, exist=found )                          
+         if( found .and. msg_flag )                                             
+     &                   call errmsg2( 26, dum, packet_file_name,               
+     &                                 dumr, dumd )                             
+         open(unit=packet_file_no, file=packet_file_name,                       
+     &        access='sequential',form='unformatted',                           
+     &        status='unknown',position='append')                               
+      end if                                                                    
+      return                                                                    
+      end                                                                       
+c     ****************************************************************          
+c     *                                                              *          
+c     *                      subroutine close_packets_file           *          
+c     *                                                              *          
+c     *                       written by : rhd                       *          
+c     *                                                              *          
+c     *                   last modified : 10/14/00                   *          
+c     *                                                              *          
+c     *                                                              *          
+c     ****************************************************************          
+c                                                                               
+c                                                                               
+c                                                                               
+      subroutine close_packets_file(msg_flag)                                   
+      use global_data ! old common.main
+      use main_data, only : packet_file_name, packet_file_no                    
+      implicit integer (a-z)                                                    
+      real dumr                                                                 
+      character :: dums                                                         
+      double precision                                                          
+     &   dumd                                                                   
+      logical connected, msg_flag                                               
+c                                                                               
+c                                                                               
+c                        open the existing binary file of packet                
+c                        results if name found in restart file.                 
+c                                                                               
+      inquire( unit=packet_file_no, opened=connected )                          
+      if ( connected ) then                                                     
+        close(unit=packet_file_no,status='keep')                                
+        if ( msg_flag )                                                         
+     &    call errmsg2( 27, dum, packet_file_name, dumr, dumd )                 
+      end if                                                                    
+c                                                                               
+      return                                                                    
+      end                                                                       
