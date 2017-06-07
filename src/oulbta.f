633fe334257b9f158183cb42f42eebd1759688eb
Robert Dodds <rhdodds@gmail.com>
2017-06-06 21:36:35 -0400

    remove commons, improve threading

--- src/oulbta.f
+++ src/oulbta.f
@@ -1,43 +1,43 @@
-c     ****************************************************************
-c     *                                                              *
-c     *                      subroutine oulbta                       *
-c     *                                                              *
-c     *                       written by : bh                        *
-c     *                                                              *
-c     *                   last modified : 08/16/89                   *
-c     *                                                              *
-c     *     this subroutine outputs the value of beta for an iter-   *
-c     *     ation of the linear pcg algorithm                        * 
-c     *                                                              *
-c     ****************************************************************
-c
-c
-c
-      subroutine oulbta(step,stpitr,iter,beta)
-      implicit integer (a-z)
-      double precision
-     &     beta        
-      logical dummy
-c
-c                       set device numbers.
-c
-      call iodevn(in,out,dummy,1)
-c
-c                       write out header
-c
-      write(out,9000) step,stpitr,iter
-c
-c                       write out parameters.
-c                       
-      write(out,9010) beta                                                          
-c
-c        
- 9000 format(///20x,'beta parameter for linear pcg algorithm'//8x,
-     &       'step number',6x,': ',i6/8x,'step iteration number : ',
-     &       i6/8x,'iteration number : ',i6//)
-c
- 9010 format(//11x,'beta parameter = ',e12.5/)
-c
-c
-      return
-      end
+c     ****************************************************************          
+c     *                                                              *          
+c     *                      subroutine oulbta                       *          
+c     *                                                              *          
+c     *                       written by : bh                        *          
+c     *                                                              *          
+c     *                   last modified : 08/16/89                   *          
+c     *                                                              *          
+c     *     this subroutine outputs the value of beta for an iter-   *          
+c     *     ation of the linear pcg algorithm                        *          
+c     *                                                              *          
+c     ****************************************************************          
+c                                                                               
+c                                                                               
+c                                                                               
+      subroutine oulbta(step,stpitr,iter,beta)                                  
+      implicit integer (a-z)                                                    
+      double precision                                                          
+     &     beta                                                                 
+      logical dummy                                                             
+c                                                                               
+c                       set device numbers.                                     
+c                                                                               
+      call iodevn(in,out,dummy,1)                                               
+c                                                                               
+c                       write out header                                        
+c                                                                               
+      write(out,9000) step,stpitr,iter                                          
+c                                                                               
+c                       write out parameters.                                   
+c                                                                               
+      write(out,9010) beta                                                      
+c                                                                               
+c                                                                               
+ 9000 format(///20x,'beta parameter for linear pcg algorithm'//8x,              
+     &       'step number',6x,': ',i6/8x,'step iteration number : ',            
+     &       i6/8x,'iteration number : ',i6//)                                  
+c                                                                               
+ 9010 format(//11x,'beta parameter = ',e12.5/)                                  
+c                                                                               
+c                                                                               
+      return                                                                    
+      end                                                                       
