36bc6555f6f9c56a9e3b404fe674dc95bc0b17c9
Robert Dodds <rhdodds@gmail.com>
2017-06-06 21:36:38 -0400

    remove commons, improve threading

--- src/thyme.f
+++ src/thyme.f
@@ -1,50 +1,50 @@
-c     ****************************************************************
-c     *                                                              *
-c     *                      subroutine thyme                        *
-c     *                                                              *
-c     *                       written by : bh                        *
-c     *                                                              *
-c     *                   last modified : 06/15/90                   *
-c     *                                                              *
-c     *     this subroutine handles the timing of various routines.  *
-c     *                                                              *
-c     ****************************************************************
-c
-c
-c
-      subroutine thyme( calc, flag )
-      implicit integer (a-z)
-      include 'common.main'
-      real  t1, wcputime
-      external wcputime
-c
-c
-      if ( slave_processor ) return
-c                    
-      if ( flag .eq. 1 ) then
-c
-c                       initial call.
-c
-         strtm = wcputime ( 1 )
-c
-      else
-c
-c                       final call
-c
-         t1            = wcputime ( 1 ) 
-         times(calc,1) = times(calc,1) + t1 - strtm
-         times(calc,2) = times(calc,2) + 1.0
-c
-      end if
-c
-c
-      return
-      end
-
-
-
-
-
-
-
-
+c     ****************************************************************          
+c     *                                                              *          
+c     *                      subroutine thyme                        *          
+c     *                                                              *          
+c     *                       written by : bh                        *          
+c     *                                                              *          
+c     *                   last modified : 06/15/90                   *          
+c     *                                                              *          
+c     *     this subroutine handles the timing of various routines.  *          
+c     *                                                              *          
+c     ****************************************************************          
+c                                                                               
+c                                                                               
+c                                                                               
+      subroutine thyme( calc, flag )                                            
+      use global_data ! old common.main
+      implicit integer (a-z)                                                    
+      real  t1, wcputime                                                        
+      external wcputime                                                         
+c                                                                               
+c                                                                               
+      if ( slave_processor ) return                                             
+c                                                                               
+      if ( flag .eq. 1 ) then                                                   
+c                                                                               
+c                       initial call.                                           
+c                                                                               
+         strtm = wcputime ( 1 )                                                 
+c                                                                               
+      else                                                                      
+c                                                                               
+c                       final call                                              
+c                                                                               
+         t1            = wcputime ( 1 )                                         
+         times(calc,1) = times(calc,1) + t1 - strtm                             
+         times(calc,2) = times(calc,2) + 1.0                                    
+c                                                                               
+      end if                                                                    
+c                                                                               
+c                                                                               
+      return                                                                    
+      end                                                                       
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
