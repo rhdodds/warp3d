7d13623ae7224b60cad5a3fc0301b2b53cf6e631
Robert Dodds <rhdodds@gmail.com>
2017-07-12 22:05:25 -0400

    testing git

--- src/zero_vol.f
+++ src/zero_vol.f
@@ -1,31 +1,31 @@
-c     ****************************************************************          
-c     *                                                              *          
-c     *                      subroutine zero_vol                     *          
-c     *                                                              *          
-c     *                       written by : kck                       *          
-c     *                                                              *          
-c     *                   last modified : 02/15/92                   *          
-c     *                                 : 02/08/94 rhd               *          
-c     *                                                              *          
-c     *     initialize volumetric terms for bbar                     *          
-c     *                                                              *          
-c     ****************************************************************          
-c                                                                               
-      subroutine zero_vol ( vol, volume, span, mxvl )                           
-      implicit integer ( a-z )                                                  
-      double precision                                                          
-     &  vol(mxvl,8,3), volume(span), zero                                       
-      data zero / 0.0d0 /                                                       
-      vol = zero                                                                
-      volume = zero                                                             
-      return                                                                    
-      end                                                                       
-                                                                                
-                                                                                
-                                                                                
-                                                                                
-                                                                                
-                                                                                
-                                                                                
-                                                                                
-                                                                                
+c     ****************************************************************
+c     *                                                              *
+c     *                      subroutine zero_vol                     *
+c     *                                                              *
+c     *                       written by : kck                       *
+c     *                                                              *
+c     *                   last modified : 02/15/92                   *
+c     *                                 : 02/08/94 rhd               *
+c     *                                                              *
+c     *     initialize volumetric terms for bbar                     *
+c     *                                                              *
+c     ****************************************************************
+c
+      subroutine zero_vol ( vol, volume, span, mxvl )
+      implicit integer ( a-z )
+      double precision
+     &  vol(mxvl,8,3), volume(span), zero
+         data zero / 0.0d0 /
+      vol = zero
+      volume = zero
+      return
+      end
+
+
+
+
+
+
+
+
+
