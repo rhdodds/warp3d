6637941e784deb5e1b0e8396e5c5db025d94a72c
Robert H. Dodds, Jr <rhdodds@gmail.com>
2019-03-11 20:59:53 -0400

    minor updates, comments

--- patwarp/compile_patwarp_windows.bat
+++ patwarp/compile_patwarp_windows.bat
@@ -9,7 +9,7 @@ cls
 ::   Build executable for patwarp utility program on Windows (64-bit mode)
 ::   and put it into the run_windows_64 directory
 ::
-::   This set up works for Intel F-90 version 11.x.
+::   This set up works for Intel F-90
 ::
 :: ****************************************************************************
 ::
@@ -33,13 +33,13 @@ ifort %fopts1% %fopts2% %files% /o patwarp.exe
 ::
   echo.
   echo ... Build done
-  echo ... Cleanup and move .exe to run_windows_64 directory
+  echo ... Cleanup and move .exe to run_windows directory
   echo.
 ::
 del .\patwarp_data.mod
 del .\patwarp_module.obj
 del .\patwarp_windows_mac.obj
-copy patwarp.exe ..\run_windows_64\patwarp_windows.exe
+copy patwarp.exe ..\run_windows\patwarp_windows.exe
 del patwarp.exe
   echo.
   echo ... All Done ....
