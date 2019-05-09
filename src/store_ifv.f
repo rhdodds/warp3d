ff7a4ac4b988fbe3ed7d7e5bd282456545f07f42
Robert H. Dodds, Jr <rhdodds@gmail.com>
2019-05-09 08:21:31 -0400

    cleanup ...

--- src/store_ifv.f
+++ src/store_ifv.f
@@ -4,8 +4,7 @@ c     *                      subroutine store_ifv                    *
 c     *                                                              *          
 c     *                       written by : ag                        *          
 c     *                                                              *          
-c     *                   last modified : 06/06/94                   *          
-c     *                   last modified : 07/29/95 (rhd)             *          
+c     *                   last modified : 05/9/2019 rhd              *          
 c     *                                                              *          
 c     *        continue to reduce forces a killed element imposes    *          
 c     *        on the structure                                      *          
@@ -20,14 +19,16 @@ c
       use main_data,         only : elems_to_blocks                             
       use damage_data, only : release_type                                      
 c                                                                               
-      implicit integer (a-z)                                                    
-      double precision                                                          
-     &     zero, one                                                            
-      logical debug                                                             
+      implicit none
+c
+      integer :: elem, elem_ptr
+      logical :: debug
+c
+      integer :: num_edof, i, blk, rel_elem                                                     
+      double precision, parameter :: zero = 0.d0,one = 1.d0
       integer, dimension(:,:), pointer :: edest                                 
-      data zero, one /0.0,1.0/                                                  
 c                                                                               
-      if ( debug ) write (*,*) '>>>>> Entering store_ifv'                       
+      if( debug ) write (*,*) '>>>>> Entering store_ifv'                       
 c                                                                               
 c            initialize state of release for element                            
 c                                                                               
@@ -48,7 +49,7 @@ c            drop the internal forces by a fraction, then release the
 c            remaining forces linearly with opening displacement.               
 c            current reduction is zero.                                         
 c                                                                               
-      if ( release_type .eq. 2 ) then                                           
+      if( release_type .eq. 2 ) then                                           
         do i = 1, num_edof                                                      
             dam_ifv(i,elem_ptr) = one * dam_ifv(i,elem_ptr)                     
         end do                                                                  
@@ -62,7 +63,7 @@ c
      &                            dam_ifv(i,elem_ptr)                           
       end do                                                                    
 c                                                                               
-      if ( debug ) then                                                         
+      if( debug ) then                                                         
          write (*,*) '>>>>> load in storeifv for elem:',elem                    
          do i = 1, num_edof                                                     
             write (*,'("new load:",i5,1x,e14.6)') edest(i,rel_elem),            
