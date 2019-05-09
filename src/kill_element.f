d2ddeae47be6d9c25c88ea97126cd06e790cad52
Robert H. Dodds, Jr <rhdodds@gmail.com>
2019-05-09 08:14:31 -0400

    cleanup for new SMCS features

--- src/kill_element.f
+++ src/kill_element.f
@@ -4,11 +4,7 @@ c     *                      subroutine kill_element                 *
 c     *                                                              *          
 c     *                       written by : ag                        *          
 c     *                                                              *          
-c     *                   last modified : 06/03/94                   *          
-c     *                   last modified : 08/13/94 rhd               *          
-c     *                   last modified : 04/03/94 asg -- don't zero *          
-c     *                                                 the porosity *          
-c     *                   last modified : 03/10/04 (rhd)             *          
+c     *                   last modified : 05/9/2019 rhd              *          
 c     *                                                              *          
 c     *        This routine kills an element in the model. the       *          
 c     *        material properties and history data for the element  *          
@@ -26,21 +22,23 @@ c
       use main_data                                                             
       use elem_block_data, only : history_blocks, urcs_n_blocks,                
      &                            urcs_n1_blocks, history_blk_list              
-      implicit integer (a-z)   
-c                                                 
-      double precision ::  zero, word                                                            
-      double precision,                                                         
-     &   dimension(:), pointer :: history, urcs_n1, urcs_n 
-      real, parameter :: rzero = 0.0                     
+      implicit none   
+c   
+      integer :: ielem
+      logical :: debug
+c 
+c              locals
+c
+      integer :: elem_type, material_type, ngp, blk, rel_elem,
+     &           hist_size, hist_offset, gp, hisloc, sig_start, 
+     &           sig_end, i                                              
+      double precision :: porosity
+      double precision, parameter :: zero = 0.d0
+      double precision, dimension(:), pointer :: history, urcs_n1,
+     &                                           urcs_n 
+      logical :: gurson, regular_material, cohesive_elem
 c                                                                               
-      integer :: iword(2)                                                          
-      logical :: debug, cohesive_elem                                              
-      equivalence ( iword , word )                                              
-      data zero / 0.0 /                                                         
-      data iword(1), iword(2) /-1,0/                                            
-c                                                                               
-c                                                                               
-      if ( debug ) write (*,*) '>>>> in kill_element'                           
+      if( debug ) write (*,*) '>>>> in kill_element'                           
 c                                                                               
 c              for regular elements (hex, tet, et.c),                           
 c              set young's modulus and poisson's ratio to zero for              
@@ -49,17 +47,19 @@ c              computes zero. for cohesive elements, we set a row of
 c              element props table = 1 so the element and cohesive model        
 c              routines know element is killed.                                 
 c                                                                               
-      elem_type = iprops(1,ielem)                                               
-      cohesive_elem = cohesive_ele_types(elem_type)                             
-      if ( .not. cohesive_elem ) then                                           
-         props(7,ielem) = rzero                                                  
-         props(8,ielem) = rzero                                                  
-      else                                                                      
-         props(7,ielem) = rzero                                                  
-         props(8,ielem) = rzero                                                  
-         props(9,ielem) = rzero                                                  
-         props(20,ielem) = rzero                                                 
-         props(21,ielem) = rzero                                                 
+      elem_type     = iprops(1,ielem)   
+      material_type = iprops(25,ielem)      
+      gurson        = material_type .eq. 3  ! also could just be mises                                      
+      regular_material = .not. gurson
+      cohesive_elem = cohesive_ele_types(elem_type)    
+c                         
+      if( .not. cohesive_elem ) then                                           
+         props(7,ielem) = 0.0    !   E and nu                                                
+         props(8,ielem) = 0.0                                                  
+      else  ! cohesive elements                                                                    
+         props(7:9,ielem) = 0.0
+         props(20,ielem)  = 0.0
+         props(21,ielem)  = 0.0
          iprops(32,ielem) = 1                                                   
       end if                                                                    
 c                                                                               
@@ -76,36 +76,40 @@ c
       history       => history_blocks(blk)%ptr                                  
       urcs_n1       => urcs_n1_blocks(blk)%ptr                                  
       urcs_n        => urcs_n_blocks(blk)%ptr                                   
-c                                                                               
-      do gp = 1, ngp                                                            
-         hisloc = hist_offset + (gp-1)*hist_size                                
-         if ( .not. cohesive_elem ) then                                        
-c                                                                               
-c             do not zero the porosity -- keep it's killed value                
-c             after it is killed                                                
-c                                                                               
-            history(hisloc+1) = zero                                            
-            history(hisloc+4) = zero                                            
-            history(hisloc+6)  = word                                           
-            history(hisloc+7)  = zero                                           
-            history(hisloc+8)  = zero                                           
-            history(hisloc+9)  = zero                                           
-            history(hisloc+10) = zero                                           
-         else                                                                   
+c    
+      if( gurson ) then ! don't zero porosity
+         do gp = 1, ngp                                                            
+           hisloc = hist_offset + (gp-1)*hist_size 
+           porosity = history(hisloc+5)                          
+           history(hisloc+1:hisloc+hist_size) = zero 
+           history(hisloc+5) = porosity  
+         end do                                         
+      end if
+c
+      if( cohesive_elem ) then
+         do gp = 1, ngp                                                            
+            hisloc = hist_offset + (gp-1)*hist_size                                
             history(hisloc+1) = zero                                            
             history(hisloc+2) = zero                                            
             history(hisloc+3) = zero                                            
-         end if                                                                 
-      end do                                                                    
+         end do                                                                    
+      end if
+c
+      if( regular_material ) then
+         do gp = 1, ngp                                                            
+           hisloc = hist_offset + (gp-1)*hist_size 
+           history(hisloc+1:hisloc+hist_size) = zero 
+         end do
+      end if
 c                                                                               
       sig_start = (rel_elem-1)*nstrs*ngp + 1                                    
       sig_end   = sig_start + ngp*nstrs - 1                                     
-      do ii = sig_start, sig_end                                                
-        urcs_n1(ii) = zero                                                      
-        urcs_n(ii)  = zero                                                      
+      do i = sig_start, sig_end                                                
+        urcs_n1(i) = zero                                                      
+        urcs_n(i)  = zero                                                      
       end do                                                                    
 c                                                                               
-      if ( debug ) write (*,*) '<<<< leaving kill_element'                      
+      if( debug ) write (*,*) '<<<< leaving kill_element'                      
 c                                                                               
       return                                                                    
       end                                                                       
@@ -115,8 +119,7 @@ c     *                      subroutine update_killed_energy         *
 c     *                                                              *          
 c     *                       written by : rhd                       *          
 c     *                                                              *          
-c     *                   last modified : 12/26/99                   *          
-c     *                                 : 01/02/01 sushovan          *          
+c     *                   last modified : 05/9/2019 rhd              *          
 c     *                                                              *          
 c     *        An element is being killed. We need to save the       *          
 c     *        internal and plastic work right now so it can be      *          
@@ -131,18 +134,19 @@ c
       use global_data ! old common.main
       use main_data, only : elems_to_blocks, cohesive_ele_types                 
       use elem_block_data, only : urcs_n_blocks, element_vol_blocks             
-      implicit integer (a-z)                                                    
-      double precision                                                          
-     &     zero, sum_internal_energy, sum_plastic_energy,                       
-     &     element_volume, rgp                                                  
-      double precision,                                                         
-     &   dimension(:), pointer :: urcs_n                                        
+      implicit none
+c
+      integer :: ielem
+c
+      integer :: elem_type, ngp, blk, rel_elem, sig_start, gp
+      double precision :: sum_internal_energy, sum_plastic_energy,                       
+     &                    element_volume, rgp                                                  
+      double precision, pointer :: urcs_n(:) 
+      double precision, parameter :: zero = 0.d0                                       
+      logical :: cohesive_elem   
+      logical, parameter :: debug = .false.                                           
 c                                                                               
-      logical debug, cohesive_elem                                              
-      data zero, debug / 0.0, .false. /                                         
-c                                                                               
-c                                                                               
-      if ( debug ) write (*,*) '>>>> in update_killed_energy'                   
+      if( debug ) write (*,*) '>>>> in update_killed_energy'                   
 c                                                                               
 c              for regular elements (hex, tet, et.c),                           
 c              compute the average internal energy and plastic energy           
@@ -152,7 +156,7 @@ c              elements.
 c                                                                               
       elem_type     = iprops(1,ielem)                                           
       cohesive_elem = cohesive_ele_types(elem_type)                             
-      if ( cohesive_elem ) return                                               
+      if( cohesive_elem ) return                                               
       ngp           = iprops(6,ielem)                                           
       blk           = elems_to_blocks(ielem,1)                                  
       rel_elem      = elems_to_blocks(ielem,2)                                  
@@ -168,7 +172,7 @@ c
         sig_start           = sig_start + nstrs                                 
       end do                                                                    
 c                                                                               
-      rgp                 = real(ngp)                                           
+      rgp                 = dble(ngp)                                           
       sum_internal_energy = sum_internal_energy / rgp                           
       sum_plastic_energy  = sum_plastic_energy  / rgp                           
 c                                                                               
@@ -185,6 +189,7 @@ c
                                                                                 
                                                                                 
                                                                                 
+                                                                                 
                                                                                 
                                                                                 
                                                                                 
