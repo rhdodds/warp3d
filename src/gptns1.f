e531fb4ca20c18d51c797c60cad2734755de4714
Robert H. Dodds, Jr <rhdodds@gmail.com>
2019-09-29 07:58:19 -0400

    add options for [Q] in ctran1 via contains. maintain use of version in manual for now. add routine to compute [cep] determinant for debugging.

--- src/gptns1.f
+++ src/gptns1.f
@@ -1728,13 +1728,19 @@ c     ****************************************************************
 c                                                                               
       subroutine ctran1( span, cep, qn1, cs, qbar, dj, w, is_umat,              
      &                   umat_stress_type, is_crys_pls  )                       
-      implicit integer (a-z)                                                    
-      include 'param_def'                                                       
-      double precision ::                                                       
-     &     cep(mxvl,nstr,*), qn1(mxvl,nstr,*), tc(mxvl,nstr,nstr),              
-     &     cs(mxvl,*), half, two, dj(*), w, wf, halfw                           
-      logical :: qbar, is_umat, is_crys_pls, do_transform                       
-      data half, two / 0.5d00, 2.0d00 /                                         
+      implicit none                                                    
+      include 'param_def'     
+c
+      integer :: span, umat_stress_type
+      logical :: qbar, is_umat, is_crys_pls
+      double precision :: cep(mxvl,nstr,*), qn1(mxvl,nstr,*),
+     &                    cs(mxvl,*),  dj(*), w                                         
+c
+      integer :: j, i
+      logical :: bar, do_transform                  
+      double precision :: tc(mxvl,nstr,nstr)
+      double precision, parameter :: zero = 0.d0, two = 2.d0,
+     &                               half = 0.5d0
 c                                                                               
 c             [cep] (mxvl x 6 x 6) relates increments                           
 c             of unrotated cauchy stress to increments                          
@@ -1883,62 +1889,345 @@ c            is: - trans([B]) * [Q-bar] * [B]. this modification of
 c            [cep] is essential for convergence of nearly homogeneous           
 c            deformation problems.                                              
 c                                                                               
-      if( qbar ) then                                                           
+      if( qbar ) call ctran1_qbar_original  !_original
+c                                                                               
+      return 
+c
+      contains
+c     ========
+c
+      subroutine ctran1_qbarv2
+      implicit none
+c
+      integer :: i, iwork(6) 
+      double precision :: q(6,6), cep_trial(6,6), det, dwork(6),
+     &                    wf, halfw, cxx, cyy, czz, cxy, cyz, cxz,
+     &                    qhat(6,6), total(6,6), total_transpose(6,6)
+!DIR$ IVDEP                                                                     
+!DIR$ VECTOR ALIGNED                                                            
+      do i = 1, span                                                          
+       wf    = dj(i) * w                                                      
+       halfw = half * wf
+       cxx = cs(i,1)
+       cyy = cs(i,2)
+       czz = cs(i,3)
+       cxy = cs(i,4)
+       cyz = cs(i,5)
+       cxz = cs(i,6)
+c
+       q(1,1) = two * cxx * wf    
+       q(2,1) = zero
+       q(3,1) = zero
+       q(4,1) = cxy * wf                                 
+       q(5,1) = zero
+       q(6,1) = cxz * wf                                 
+c
+       q(1,2) = zero
+       q(2,2) = two * cyy * wf                           
+       q(3,2) = zero
+       q(4,2) = cxy * wf                                 
+       q(5,2) = cyz * wf                                 
+       q(6,2) = zero
+c
+       q(1,3) = zero
+       q(2,3) = zero
+       q(3,3) = two * czz * wf                           
+       q(4,3) = zero
+       q(5,3) = cyz * wf                                 
+       q(6,3) = cxz * wf  
+c                
+       q(1,4) = cxy * wf                                              
+       q(2,4) = cxy * wf                                                
+       q(3,4) = zero
+       q(4,4) = halfw * ( cxx + cyy )                  
+       q(5,4) = wf * cxz                              
+       q(6,4) = wf * cyz     
+c
+       q(1,5) = zero
+       q(2,5) = cyz * wf                                               
+       q(3,5) = cyz * wf                                               
+       q(4,5) = cxz * wf                                               
+       q(5,5) = halfw * ( cyy + czz )                  
+       q(6,5) = wf * cxy          
+                
+       q(1,6) = cxz * wf                                                 
+       q(2,6) = zero                                                 
+       q(3,6) = cxz * wf                                                
+       q(4,6) = cyz * wf                                                 
+       q(5,6) = cxy * wf    
+       q(6,6) = halfw * ( cxx + czz )     
+c
+       qhat(1:6,1) =  cs(i,1:6) * wf
+       qhat(1:6,2) =  cs(i,1:6) * wf
+       qhat(1:6,3) =  cs(i,1:6) * wf
+       qhat(1:6,4:6) = zero
+c 
+       total = -q + qhat
+       total_transpose = transpose( total ) 
+       total = half * ( total + total_transpose )
+c
+       cep(i,1:6,1:6) = cep(i,1:6,1:6) + total(1:6,1:6) 
+!       call dgedi( cep_trial, 6, 6, iwork, det, dwork, 10 )
+!       if( det <=zero ) then
+!        write(*,*) " @ 1 det <=0"
+!       end if
+!        return
+!       else
+!           cep(i,1:6,1:6) = cep_trial
+!       end if      
+      end do                                                                                                                                            
+c
+      return
+      end subroutine ctran1_qbarv2
+c
+      subroutine ctran1_qbarv1
+      implicit none
+c
+      integer :: i, iwork(6) 
+      double precision :: q(6,6), cep_trial(6,6), det, dwork(6),
+     &                    wf, halfw, cxx, cyy, czz, cxy, cyz, cxz
+c
 !DIR$ IVDEP                                                                     
 !DIR$ VECTOR ALIGNED                                                            
         do i = 1, span                                                          
          wf    = dj(i) * w                                                      
-         halfw = half * wf                                                      
-         cep(i,1,1) = cep(i,1,1) - two * cs(i,1) * wf                           
-         cep(i,2,2) = cep(i,2,2) - two * cs(i,2) * wf                           
-         cep(i,3,3) = cep(i,3,3) - two * cs(i,3) * wf                           
-         cep(i,4,1) = cep(i,4,1) - cs(i,4) * wf                                 
-         cep(i,6,1) = cep(i,6,1) - cs(i,6) * wf                                 
-         cep(i,4,2) = cep(i,4,2) - cs(i,4) * wf                                 
-         cep(i,5,2) = cep(i,5,2) - cs(i,5) * wf                                 
-         cep(i,5,3) = cep(i,5,3) - cs(i,5) * wf                                 
-         cep(i,6,3) = cep(i,6,3) - cs(i,6) * wf                                 
-         cep(i,4,4) = cep(i,4,4) - halfw * ( cs(i,1)+cs(i,2) )                  
-         cep(i,5,5) = cep(i,5,5) - halfw * ( cs(i,2)+cs(i,3) )                  
-         cep(i,6,6) = cep(i,6,6) - halfw * ( cs(i,1)+cs(i,3) )                  
-         cep(i,5,4) = cep(i,5,4) - halfw * cs(i,6)                              
-         cep(i,6,4) = cep(i,6,4) - halfw * cs(i,5)                              
-         cep(i,6,5) = cep(i,6,5) - halfw * cs(i,4)                              
-         cep(i,1,4) = cep(i,4,1)                                                
-         cep(i,1,6) = cep(i,6,1)                                                
-         cep(i,2,4) = cep(i,4,2)                                                
-         cep(i,2,5) = cep(i,5,2)                                                
-         cep(i,3,5) = cep(i,5,3)                                                
-         cep(i,3,6) = cep(i,6,3)                                                
-         cep(i,4,5) = cep(i,5,4)                                                
-         cep(i,4,6) = cep(i,6,4)                                                
-         cep(i,5,6) = cep(i,6,5)                                                
-c                                                                               
-c                      experiment with symmetrized version of the               
-c                      nonsymmetric term. see Crisfield vol. 2, pg55.           
-c                                                                               
-c      z(1:6,1:6) = 0.0d0                                                       
-C      z(1,1:3) = cs(i,1)                                                       
-C      z(2,1:3) = cs(i,2)                                                       
-C      z(3,1:3) = cs(i,3)                                                       
-C      z(4,1:3) = cs(i,4)                                                       
-C      z(5,1:3) = cs(i,5)                                                       
-C      z(6,1:3) = cs(i,6)                                                       
-c      zt = transpose( z)                                                       
-c      do k = 1, 6                                                              
-c      do l = 1, 6                                                              
-c        z(k,l) = 0.5d0*(z(k,l)+zt(l,k))                                        
-c        cep(i,k,l) = cep(i,k,l) + z(k,l)*wf                                    
-c      end do                                                                   
-c      end do                                                                   
-c                                                                               
-        end do                                                                  
-      end if                                                                    
-c                                                                               
-      return                                                                    
-      end                                                                       
+         halfw = half * wf
+c
+         cxx = cs(i,1)
+         cyy = cs(i,2)
+         czz = cs(i,3)
+         cxy = cs(i,4)
+         cyz = cs(i,5)
+         cxz = cs(i,6)
+c
+         q(1,1) = two * cxx * wf    
+         q(2,1) = zero
+         q(3,1) = zero
+         q(4,1) = cxy * wf                                 
+         q(5,1) = zero
+         q(6,1) = cxz * wf                                 
+c
+         q(1,2) = zero
+         q(2,2) = two * cyy * wf                           
+         q(3,2) = zero
+         q(4,2) = cxy * wf                                 
+         q(5,2) = cyz * wf                                 
+         q(6,2) = zero
+c
+         q(1,3) = zero
+         q(2,3) = zero
+         q(3,3) = two * czz * wf                           
+         q(4,3) = zero
+         q(5,3) = cyz * wf                                 
+         q(6,3) = cxz * wf  
+c                  
+         q(1,4) = cxy * wf                                              
+         q(2,4) = cxy * wf                                                
+         q(3,4) = zero
+         q(4,4) = halfw * ( cxx + cyy )                  
+         q(5,4) = wf * cxz                              
+         q(6,4) = wf * cyz     
+c
+         q(1,5) = zero
+         q(2,5) = cyz * wf                                               
+         q(3,5) = cyz * wf                                               
+         q(4,5) = cxz * wf                                               
+         q(5,5) = halfw * ( cyy + czz )                  
+         q(6,5) = wf * cxy          
+                  
+         q(1,6) = cxz * wf                                                 
+         q(2,6) = zero                                                 
+         q(3,6) = cxz * wf                                                
+         q(4,6) = cyz * wf                                                 
+         q(5,6) = cxy * wf    
+         q(6,6) = halfw * ( cxx + czz )     
+c
+         cep(i,1:6,1:6) = cep(i,1:6,1:6) - q(1:6,1:6)
+!         call warp3d_dgedi( cep_trial, 6, 6, iwork, det, dwork, 10 )
+!         if( det <=zero ) then
+!          write(*,*) " @ 1 det <=0"
+!         end if
+!          return
+!         else
+!             cep(i,1:6,1:6) = cep_trial
+!        end if      
+!
+      end do                                                                                                                                            
+c
+      return
+      end subroutine ctran1_qbarv1
+c
+      subroutine ctran1_qbar_original
+      implicit none
+c
+      integer :: i
+      double precision :: wf, halfw
+c
+!DIR$ IVDEP                                                                     
+!DIR$ VECTOR ALIGNED                                                            
+      do i = 1, span                                                          
+       wf    = dj(i) * w                                                      
+       halfw = half * wf
+       cep(i,1,1) = cep(i,1,1) - two * cs(i,1) * wf
+       cep(i,2,2) = cep(i,2,2) - two * cs(i,2) * wf
+       cep(i,3,3) = cep(i,3,3) - two * cs(i,3) * wf
+       cep(i,4,1) = cep(i,4,1) - cs(i,4) * wf
+       cep(i,6,1) = cep(i,6,1) - cs(i,6) * wf
+       cep(i,4,2) = cep(i,4,2) - cs(i,4) * wf
+       cep(i,5,2) = cep(i,5,2) - cs(i,5) * wf
+       cep(i,5,3) = cep(i,5,3) - cs(i,5) * wf
+       cep(i,6,3) = cep(i,6,3) - cs(i,6) * wf
+       cep(i,4,4) = cep(i,4,4) - halfw * ( cs(i,1)+cs(i,2) )
+       cep(i,5,5) = cep(i,5,5) - halfw * ( cs(i,2)+cs(i,3) )
+       cep(i,6,6) = cep(i,6,6) - halfw * ( cs(i,1)+cs(i,3) )
+       cep(i,5,4) = cep(i,5,4) - halfw * cs(i,6)
+       cep(i,6,4) = cep(i,6,4) - halfw * cs(i,5)
+       cep(i,6,5) = cep(i,6,5) - halfw * cs(i,4)
+       cep(i,1,4) = cep(i,4,1)
+       cep(i,1,6) = cep(i,6,1)
+       cep(i,2,4) = cep(i,4,2)
+       cep(i,2,5) = cep(i,5,2)
+       cep(i,3,5) = cep(i,5,3)
+       cep(i,3,6) = cep(i,6,3)
+       cep(i,4,5) = cep(i,5,4)
+       cep(i,4,6) = cep(i,6,4)
+       cep(i,5,6) = cep(i,6,5)
+      end do
+c
+      return
+      end subroutine ctran1_qbar_original
+c
+      end  subroutine ctran1                                                                  
                                                                                 
                                                                                 
+      subroutine warp3d_dgedi(a,lda,n,ipvt,det,work,job)
+      integer lda,n,ipvt(1),job
+      double precision a(lda,1),det(2),work(1)
+c
+c     dgedi computes the determinant and inverse of a matrix
+c     using the factors computed by dgeco or dgefa.
+c
+c     on entry
+c
+c        a       double precision(lda, n)
+c                the output from dgeco or dgefa.
+c
+c        lda     integer
+c                the leading dimension of the array  a .
+c
+c        n       integer
+c                the order of the matrix  a .
+c
+c        ipvt    integer(n)
+c                the pivot vector from dgeco or dgefa.
+c
+c        work    double precision(n)
+c                work vector.  contents destroyed.
+c
+c        job     integer
+c                = 11   both determinant and inverse.
+c                = 01   inverse only.
+c                = 10   determinant only.
+c
+c     on return
+c
+c        a       inverse of original matrix if requested.
+c                otherwise unchanged.
+c
+c        det     double precision(2)
+c                determinant of original matrix if requested.
+c                otherwise not referenced.
+c                determinant = det(1) * 10.0**det(2)
+c                with  1.0 .le. dabs(det(1)) .lt. 10.0
+c                or  det(1) .eq. 0.0 .
+c
+c     error condition
+c
+c        a division by zero will occur if the input factor contains
+c        a zero on the diagonal and the inverse is requested.
+c        it will not occur if the subroutines are called correctly
+c        and if dgeco has set rcond .gt. 0.0 or dgefa has set
+c        info .eq. 0 .
+c
+c     linpack. this version dated 08/14/78 .
+c     cleve moler, university of new mexico, argonne national lab.
+c
+c     subroutines and functions
+c
+c     blas daxpy,dscal,dswap
+c     fortran dabs,mod
+c
+c     internal variables
+c
+      double precision t
+      double precision ten
+      integer i,j,k,kb,kp1,l,nm1
+c
+c
+c     compute determinant
+c
+      if (job/10 .eq. 0) go to 70
+         det(1) = 1.0d0
+         det(2) = 0.0d0
+         ten = 10.0d0
+         do 50 i = 1, n
+            if (ipvt(i) .ne. i) det(1) = -det(1)
+            det(1) = a(i,i)*det(1)
+c        ...exit
+            if (det(1) .eq. 0.0d0) go to 60
+   10       if (dabs(det(1)) .ge. 1.0d0) go to 20
+               det(1) = ten*det(1)
+               det(2) = det(2) - 1.0d0
+            go to 10
+   20       continue
+   30       if (dabs(det(1)) .lt. ten) go to 40
+               det(1) = det(1)/ten
+               det(2) = det(2) + 1.0d0
+            go to 30
+   40       continue
+   50    continue
+   60    continue
+   70 continue
+c
+c     compute inverse(u)
+c
+      if (mod(job,10) .eq. 0) go to 150
+         do 100 k = 1, n
+            a(k,k) = 1.0d0/a(k,k)
+            t = -a(k,k)
+            call dscal(k-1,t,a(1,k),1)
+            kp1 = k + 1
+            if (n .lt. kp1) go to 90
+            do 80 j = kp1, n
+               t = a(k,j)
+               a(k,j) = 0.0d0
+               call daxpy(k,t,a(1,k),1,a(1,j),1)
+   80       continue
+   90       continue
+  100    continue
+c
+c        form inverse(u)*inverse(l)
+c
+         nm1 = n - 1
+         if (nm1 .lt. 1) go to 140
+         do 130 kb = 1, nm1
+            k = n - kb
+            kp1 = k + 1
+            do 110 i = kp1, n
+               work(i) = a(i,k)
+               a(i,k) = 0.0d0
+  110       continue
+            do 120 j = kp1, n
+               t = work(j)
+               call daxpy(n,t,a(1,j),1,a(1,k),1)
+  120       continue
+            l = ipvt(k)
+            if (l .ne. k) call dswap(n,a(1,k),1,a(1,l),1)
+  130    continue
+  140    continue
+  150 continue
+      return
+      end
                                                                                 
                                                                                 
                                                                                 
