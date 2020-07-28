a62fa10fd447fe9c21af82ab604e46dc061b7834
Robert H. Dodds, Jr <rhdodds@gmail.com>
2020-07-28 17:08:41 -0400

    new files/ one inout file for each loading

---
+++ manual_examples_chpt1/example_3_linear/cleanup.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#           cleanup directory after running analysis
+#
+/bin/rm -f  woutput* model.text wem* wes* wnd* wns* states_head*   >& /dev/null
+/bin/rm -f  energy *~ *-e *neutral*   >& /dev/null
+#
+echo "> done..."
+exit
