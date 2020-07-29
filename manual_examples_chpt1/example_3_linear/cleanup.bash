1ca8d14b303f7af652cd4f50d9395ec6aff5da54
Robert H. Dodds, Jr <rhdodds@gmail.com>
2020-07-28 21:35:42 -0400

    don not delete output files

--- manual_examples_chpt1/example_3_linear/cleanup.bash
+++ manual_examples_chpt1/example_3_linear/cleanup.bash
@@ -2,7 +2,7 @@
 #
 #           cleanup directory after running analysis
 #
-/bin/rm -f  woutput* model.text wem* wes* wnd* wns* states_head*   >& /dev/null
+/bin/rm -f  model.text wem* wes* wnd* wns* states_head*   >& /dev/null
 /bin/rm -f  energy *~ *-e *neutral*   >& /dev/null
 #
 echo "> done..."
