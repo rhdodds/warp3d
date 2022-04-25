c8d00eb90993e83fa119729f09c23b108dc65460
Bob Dodds <rhdodds@gmail.com>
2022-04-25 13:18:36 -0600

    replacement files

---
+++ fracture_models/ssy_growth_plastic_strain_coarse_mesh/cleanup.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+\rm wnd*  >& /dev/null
+\rm wns* >& /dev/null
+\rm wnt*  >& /dev/null
+\rm wem*  >& /dev/null
+\rm wes*  >& /dev/null
+\rm state* *batch*mess* save* energy >& /dev/null
+\rm wnd* wns* wnt* wnt* wem* wes* state* *batch*mess* save* energy >& /dev/null
+\rm wnd* wns* wnt* wnt* wem* wes* state* *batch*mess* save* energy >& /dev/null
+\rm wnd* wns* wnt* wnt* wem* wes* state* *batch*mess* save* energy >& /dev/null
+\rm wnd* wns* wnt* wnt* wem* wes* state* *batch*mess* save* energy >& /dev/null
+\rm states*  results*  >& /dev/null
+\rm *text   >& /dev/null
+\rm crack_growth_values.dat fully_killed_ele_list.out  >& /dev/null
+\rm delete_elements_data.out kill_limit_restart.db >& /dev/null
+exit
