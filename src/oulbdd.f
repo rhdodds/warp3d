a54f2416ee75cf5340fc90ab894793bef4f3a0fd
Robert H. Dodds <rhdodds@gmail.com>
2025-09-20 21:41:08 -0600

    add mean, triaxiality, Lode output. major cleanup

--- src/oulbdd.f
+++ src/oulbdd.f
@@ -4,7 +4,7 @@ c     *                      subroutine oulbdd                       *
 c     *                                                              *          
 c     *                       written by : bh                        *          
 c     *                                                              *          
-c     *                   last modified : 10/30/2012                 *          
+c     *                   last modified : 8/8/25 rhd                 *          
 c     *                                                              *          
 c     *     set the label type and corresponding                     *          
 c     *     column labels for a node for displ/vel/acc/temp output   *          
@@ -16,12 +16,16 @@ c
 c                                                                               
 c                                                                               
       subroutine oulbdd( dva, hedtyp, lbltyp, type, elem, doflbl )              
-      implicit integer (a-z)                                                    
+c
+      implicit none
+c      
+      integer :: lbltyp, type, elem
       character(len=8) :: doflbl(*)                                             
-      character(len=20) :: hedtyp                                               
-      real dumr                                                                 
-      double precision                                                          
-     &     dumd                                                                 
+      character(len=20) :: hedtyp 
+c
+      integer :: dva                                                    
+      real :: dumr                                                                 
+      double precision :: dumd                                                                 
       character :: dums                                                         
 c                                                                               
 c                       branch on element type.                                 
